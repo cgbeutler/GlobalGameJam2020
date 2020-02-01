c6adcfd15d79460eea159440264fb2d20c4504e9
Will Whiteneck <will.whiteneck@gmail.com>
2020-02-01 12:06:32 -0700

    hand cursor

--- Scripts/Cursor.gd
+++ Scripts/Cursor.gd
@@ -3,6 +3,13 @@ extends Area2D
 var grabbed_obj : Grabbable
 var grabbed_obj_offset
 
+var open_hand_sprite : Sprite
+var closed_hand_sprite : Sprite
+
+func _ready():
+	open_hand_sprite = get_node("Open_Hand_Sprite")
+	closed_hand_sprite = get_node("Closed_Hand_Sprite")
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func __grab_topmost():
 	var areas = get_overlapping_areas()
@@ -24,6 +31,7 @@ func __release():
 	grabbed_obj.z_index = 0
 	grabbed_obj._on_release()
 	grabbed_obj = null
+	
 
 
 func _process( _delta : float ) -> void:
@@ -35,6 +43,11 @@ func _process( _delta : float ) -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
 		if event.pressed:
+			open_hand_sprite.hide()
+			closed_hand_sprite.show()
 			__grab_topmost()
 		else:
+			open_hand_sprite.show()
+			closed_hand_sprite.hide()
 			__release()
+
