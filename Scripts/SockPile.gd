6ea3518a07dc0377e791d36afcedf41968fb2c62
Cory Beutler <cg.beutler@gmail.com>
2020-02-01 00:21:47 -0700

    Sock count added to sock pile

--- Scripts/SockPile.gd
+++ Scripts/SockPile.gd
@@ -1,11 +1,21 @@
 extends Node
+var Sock = preload("Sock.gd")
 
+var starting_sock_count : int
+var sock_count : int
+
+func _ready() -> void:
+	for child in get_children():
+		if child is Sock:
+			starting_sock_count += 1
+	sock_count = starting_sock_count
 
 func collect_pair( sock_1, sock_2 ):
 	# TODO: Spawn congrats message or something?
 	sock_1.queue_free()
 	sock_2.queue_free()
+	sock_count -= 2
 	
-	if get_child_count() == 2:
+	if sock_count == 0:
 		print( "You Win!" )
 		get_tree().quit()
