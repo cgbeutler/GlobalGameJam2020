6a520a7285f5b1fa2790015c1406e36565daa5c1
Cory Beutler <cg.beutler@gmail.com>
2020-01-31 21:50:37 -0700

    Initial Commit

---
+++ Scripts/Cursor.gd
@@ -0,0 +1,44 @@
+extends Area2D
+var Sock = preload("Sock.gd")
+
+
+var sock
+var sock_offset
+
+
+func __grab_topmost_sock():
+	var areas = get_overlapping_areas()
+	sock = null
+	for area in areas:
+		if area is Sock:
+			if not sock:  sock = area
+			else:
+				if area.get_index() > sock.get_index():
+					sock = area
+	
+	if sock:
+		print( "you grabbed sock!" )
+		sock_offset = sock.position - position
+		sock.z_index = 1
+
+func __release_sock():
+	print( "there is no sock. only zuul" )
+	if not sock:  return
+	sock.z_index = 0
+	sock.drop()
+	sock = null
+
+
+func _process( _delta : float ) -> void:
+	# Move self to the mouse's position
+	position = get_viewport().get_mouse_position()
+	# Move sock relative to cursor
+	if sock:  sock.position = position + sock_offset
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+		if event.pressed:
+			__grab_topmost_sock()
+		else:
+			__release_sock()
+
---
+++ Scripts/Sock.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+export(int) var sock_type := 0
+
+# Called by cursor when a sock is dropped
+func drop():
+	var areas = get_overlapping_areas()
+	for area in areas:
+		if area.get( "sock_type" ) == sock_type:
+			print("yay") 
