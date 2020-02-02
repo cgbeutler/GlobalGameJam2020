da3f5d61308d187ab78be64dbd6e69acf5b68148
Dallin <DallinBeutler@gmail.com>
2020-02-02 15:51:42 -0700

    fixed some text issues

--- DatingSim/MainContainer.gd
+++ DatingSim/MainContainer.gd
@@ -9,3 +9,19 @@ signal trigger(eventName)
 
 func _on_VBoxContainer_triggered(message):
 	print("triggered " + message)
+	if (message == "pair on"):
+		$Button.disabled = false
+	pass # Replace with function body.
+
+func _on_Control_trigger(eventName):
+	print("PAIRED")
+	if (eventName == "pair on"):
+		$Button.disabled = false
+	pass # Replace with function body.
+
+
+# VICTORY!!!
+func _on_Button_pressed():
+	print("WINNNN!")
+	get_tree().change_scene("res://YouWin.tscn")
+	pass # Replace with function body.
