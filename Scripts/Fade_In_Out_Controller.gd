e74537a2f87dc1d8084ed0ea1baf05a979406320
Will Whiteneck <will.whiteneck@gmail.com>
2020-02-02 10:35:28 -0700

    fade in/out

--- Scripts/Fade_In_Out_Controller.gd
+++ Scripts/Fade_In_Out_Controller.gd
@@ -7,24 +7,28 @@ var time = 0
 
 func _process(delta):
 	time += delta
-	
-	if time > 6:
-		show()
+	if time > 10:
 		fade_out = true
-	
-	if fade_in:
-		var mat = get_surface_material(0)
-		if mat.albedo_color.a <= 0:
-			fade_in = false
-			hide()
-		else:
-			mat.albedo_color.a -= 0.005
-			set_surface_material(0, mat)
+		show()
+
+	if fade_in && time > 2:
+		fadeIn(time)
 	elif fade_out:
-		var mat = get_surface_material(0)
-		if mat.albedo_color.a >= 1:
-			fade_out = false
-		else:
-			mat.albedo_color.a += 0.008
-			set_surface_material(0, mat)
-		
+		fadeOut(time)
+
+func fadeOut(time):
+	var mat = get_surface_material(0)
+	if mat.albedo_color.a >= 1:
+		fade_out = false
+	else:
+		mat.albedo_color.a += 0.01
+		set_surface_material(0, mat)
+
+func fadeIn(time):
+	var mat = get_surface_material(0)
+	if mat.albedo_color.a <= 0:
+		fade_in = false
+		hide()
+	else:
+		mat.albedo_color.a -= 0.01
+		set_surface_material(0, mat)
--- Scripts/box_flock_behavior.gd
+++ Scripts/box_flock_behavior.gd
@@ -1,14 +1,18 @@
 extends CSGBox
 
+var time_to_start = 4
+var time = 0
 var startY
 
 func _ready():
 	startY = translation.y
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	if translation.x < -15:
-		translation.x = 15
-	else:
-		translation.x -= 0.06
-		translation.y = startY + sin(translation.x)
+func _process(delta):
+	time += delta
+	if time > time_to_start:
+		if translation.x < -15:
+			translation.x = 15
+		else:
+			translation.x -= 0.06
+			translation.y = startY + sin(translation.x)
